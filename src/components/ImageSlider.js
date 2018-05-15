37e688970779ee05b151df4e705fee144a81525e
adamtowers <hello@adamtowers.design>
2018-05-15 17:45:27 -0400

    Done.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,16 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+
+export default class ImageSlider extends React.Component {
+  state = {
+    currentSlideIndex: 0
+  }
+
+  render() {
+    return (
+      <button className="slider">
+        I am on slide {this.state.currentSlideIndex}
+      </button>
+    )
+  }
+}
